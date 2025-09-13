f601bad645b28b15d62ead0fd2f725b7ce20015b
Leonardo Cuba Ferraz <97860586+leoacferraz@users.noreply.github.com>
2025-09-13 08:32:24 -0300

    Update main.server.ts

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,9 +1,9 @@
+// src/main.server.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config.server';
-import { BootstrapContext } from '@angular/platform-server';
 
-export default function bootstrap(context: BootstrapContext) {
-  // Passa o "context" para o bootstrap no servidor
-  return bootstrapApplication(AppComponent, appConfig, { context });
+export default function bootstrap() {
+  return bootstrapApplication(AppComponent, appConfig);
 }
+
