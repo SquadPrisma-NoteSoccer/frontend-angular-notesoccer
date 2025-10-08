ed0d5df2f44938b2e81119eb4a7b307212dfcc0a
Marcos <maik.cok14@gmail.com>
2025-10-08 09:07:50 -0300

    Criação da tela de cadastro de time +  atualização de algumas classes para uma melhor leitura

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { WelcomeComponent } from './features/auth/pages/welcome/welcome.component';
 import { SignupOrganizerComponent } from './features/auth/pages/signup-organizer/signup-organizer.component';
 import { SignupSuccessComponent } from './features/auth/pages/signup-success/signup-success.component';
+import { SignupTeamComponent } from './features/auth/pages/signup-team/signup-team.component';
 
 //configuração de roteamento entre telas
 export const routes: Routes = [
   { path: '', component: WelcomeComponent},
   {path: 'signup', component: SignupOrganizerComponent},
-  {path: 'signup-success', component: SignupSuccessComponent}
+  {path: 'signup-success', component: SignupSuccessComponent},
+  {path: 'signup-team', component: SignupTeamComponent}
 ];
---
+++ src/app/features/auth/pages/signup-team/signup-team.component.ts
@@ -0,0 +1,62 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-signup-team',
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
+  templateUrl: './signup-team.component.html',
+  styleUrl: './signup-team.component.css'
+})
+export class SignupTeamComponent {
+
+  signupTeamForm: FormGroup;
+  teamName: string = ''; // nome digitado no input
+  teams: string[] = []; // lista de times
+  maxTeams = 20;
+
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.signupTeamForm = this.fb.group({
+      teamName: ['', [
+        Validators.required,
+        Validators.pattern(/^[A-Za-zÀ-ÿ]+(?: [A-Za-zÀ-ÿ]+)*$/),
+        Validators.minLength(3),
+        Validators.maxLength(20)
+      ]]
+    });
+  }
+
+  // Método para adicionar o time à lista
+  addTeam() {
+    if (this.signupTeamForm.invalid) return;
+
+    const name = this.signupTeamForm.get('teamName')?.value.trim();
+
+    // codigo para impedir de burlar com apenas espaços ou letras insuficientes
+    const cleanName = name.replace(/\s+/g, ''); // remove espaços para contar letras reais
+    if (cleanName.length < 3) {
+      alert('O nome deve ter pelo menos 3 letras válidas.');
+      return;
+    }
+
+    this.teams.push(name);
+    this.signupTeamForm.reset();
+  }
+
+  //metodo para remover os times da lista
+  removeTeam(index: number) {
+    this.teams.splice(index, 1);
+  }
+
+  //metodo para voltar para a tela anterior
+  goBack() {
+    this.router.navigate(['signup-success']);
+  }
+
+  //metodo para voltar para a tela de boas-vindas
+  goWelcome() {
+    this.router.navigate(['']);
+  }
+}
