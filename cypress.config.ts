22dfa31f85584dd1698cfc2251e3e2a6eb0afd6c
Vitor Fantin <96424825+vitorfantin@users.noreply.github.com>
2025-11-05 12:43:57 -0300

    finalização estrutura cypress junto att v15.05  e add mais itens para o git ignore (#24)

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4201',
+    defaultCommandTimeout: 10000, // pode esperar até 10 segundos por comando
+    video: true,
+    screenshotOnRunFailure: true, // tira print automático em falhas
+    trashAssetsBeforeRuns: true, // limpa vídeos antigos antes de rodar
+    videoCompression: 32, // (opcional) reduz tamanho dos vídeos
+    videosFolder: "cypress/videos", 
+    screenshotsFolder: "cypress/screenshots",
+
     supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
