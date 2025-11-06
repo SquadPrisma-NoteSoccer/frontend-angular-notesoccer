fdb9e298ceed38b1529b40ac885a293b98e1f7d9
Vitor Fantin <96424825+vitorfantin@users.noreply.github.com>
2025-11-06 10:57:51 -0300

    Alteração do cypress para javaScript (#27)

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,17 @@
-import { defineConfig } from 'cypress'
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4201',
-    projectId: process.env['CYPRESS_PROJECT_ID'],    
+    projectId: process.env['CYPRESS_PROJECT_ID'],
     defaultCommandTimeout: 10000, // pode esperar até 10 segundos por comando
     video: true,
     screenshotOnRunFailure: true, // tira print automático em falhas
     trashAssetsBeforeRuns: true, // limpa vídeos antigos antes de rodar
     videoCompression: 32, // (opcional) reduz tamanho dos vídeos
-    videosFolder: "cypress/videos", 
+    videosFolder: "cypress/videos",
     screenshotsFolder: "cypress/screenshots",
-
-    supportFile: false,
+    
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
---
+++ cypress/e2e/web/tests/exemplo.cy.js
@@ -0,0 +1,5 @@
+describe('Exemplo Teste Automação', () => {
+    it('Caso de teste', () => {
+        cy.visit('/');
+    });
+});
