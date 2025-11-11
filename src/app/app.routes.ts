bfa56de50dca3790aad8b9d3e954f398d466b88d
Vitor Fantin <96424825+vitorfantin@users.noreply.github.com>
2025-11-11 17:45:16 -0300

    Merge remote-tracking branch 'origin/develop' into staging (#44)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,15 @@
 import { Routes } from '@angular/router';
-import { WelcomeComponent } from './features/auth/pages/welcome/welcome.component';
-import { SignupOrganizerComponent } from './features/auth/pages/signup-organizer/signup-organizer.component';
-import { SignupSuccessComponent } from './features/auth/pages/signup-success/signup-success.component';
-import { SignupTeamComponent } from './features/auth/pages/signup-team/signup-team.component';
+import { WelcomeComponent } from './features/pages/welcome/welcome.component';
+import { SignupOrganizerComponent } from './features/pages/signup-organizer/signup-organizer.component';
+import { SignupSuccessComponent } from './features/pages/signup-success/signup-success.component';
+import { SignupTeamComponent } from './features/pages/signup-team/signup-team.component';
+import { LoginOrganizerComponent } from './features/pages/login-organizer/login-organizer.component';
 
 //configuração de roteamento entre telas
 export const routes: Routes = [
   { path: '', component: WelcomeComponent},
   {path: 'signup', component: SignupOrganizerComponent},
   {path: 'signup-success', component: SignupSuccessComponent},
+  {path: 'login-organizer', component: LoginOrganizerComponent},
   {path: 'signup-team', component: SignupTeamComponent}
 ];
---
+++ src/app/features/services/signup-organizer.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Organizer } from '../models/organizer';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SignupOrganizerService {
+  private url = `${environment.apiUrl}/api/v1/orquestrador/usuarios`;
+
+  constructor(private http: HttpClient) {}
+
+  registerOrganizer(organizer: Organizer): Observable<Organizer> {
+    return this.http.post<Organizer>(this.url, organizer);
+  }
+}
