a35917b46682fc23ff7a0624fb154f5137305ee1
Leonardo A. C. Ferraz <leoalvo89@gmail.com>
2025-11-11 11:44:39 -0300

    chore(env): adiciona fallback local e suporte à variável NG_APP_API_BASE_URL

--- src/app/features/services/signup-organizer.service.ts
+++ src/app/features/services/signup-organizer.service.ts
@@ -2,16 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Organizer } from '../models/organizer';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SignupOrganizerService {
+  private url = `${environment.apiUrl}/api/v1/orquestrador/usuarios`;
 
-  private url = 'https://orchestration-service-dev.onrender.com/api/v1/orquestrador/usuarios';
-
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   registerOrganizer(organizer: Organizer): Observable<Organizer> {
     return this.http.post<Organizer>(this.url, organizer);
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: import.meta.env.NG_APP_API_BASE_URL || 'http://localhost:8080',
+  // apiUrl: 'https://drainage-joke-territories-incorporate.trycloudflare.com',
+};
