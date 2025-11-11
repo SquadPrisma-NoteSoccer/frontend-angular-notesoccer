1691e7c8f010f3c0c8b4e60416a285f09fdd5489
Leonardo Cuba Ferraz <97860586+leoacferraz@users.noreply.github.com>
2025-11-11 15:45:18 -0300

    Update API URL configuration in environment.ts
    Remove default API URL fallback in environment configuration.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: (window as any).__env?.NG_APP_API_BASE_URL ?? 'http://localhost:8080',
+  apiUrl: (window as any).__env?.NG_APP_API_BASE_URL,
   // apiUrl: 'https://drainage-joke-territories-incorporate.trycloudflare.com',
 };
