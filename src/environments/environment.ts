db98219fafd254946c98d88404653cc671ff5321
Leonardo A. C. Ferraz <leoalvo89@gmail.com>
2025-11-11 12:02:51 -0300

    fix: adicionado variável
    This reverts commit bf535b2038be8cc4ebc33ef9f078a31c313ba818.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: (window as any).__env?.NG_APP_API_BASE_URL ?? 'http://localhost:8080',
+  apiUrl: import.meta.env.NG_APP_API_BASE_URL || 'http://localhost:8080',
   // apiUrl: 'https://drainage-joke-territories-incorporate.trycloudflare.com',
 };
