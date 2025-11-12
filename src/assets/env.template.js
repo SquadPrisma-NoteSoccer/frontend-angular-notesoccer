d20ff6d36d6d7945909aa3110cdc138eacfe8202
Leonardo Cuba Ferraz <97860586+leoacferraz@users.noreply.github.com>
2025-11-12 11:46:21 -0300

    Update env.template.js to use placeholder syntax

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -1,2 +1,4 @@
-window.__env = window.__env || {};
-window.__env.NG_APP_API_BASE_URL = "$NG_APP_API_BASE_URL";
+(function (window) {
+  window.__env = window.__env || {};
+  window.__env.NG_APP_API_BASE_URL = '##NG_APP_API_BASE_URL##';
+})(this);
