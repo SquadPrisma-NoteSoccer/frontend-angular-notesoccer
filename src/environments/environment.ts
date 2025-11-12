d577df05f8771c50b4756de1f2d0b96d55b46af9
Leonardo Cuba Ferraz <97860586+leoacferraz@users.noreply.github.com>
2025-11-12 15:20:33 -0300

    Configuração de variável dinâmica NG_APP_API_BASE_URL via Netlify UI (#48)
    * feat(frontend): configurar injeção dinâmica da variável NG_APP_API_BASE_URL via Netlify UI
    
    * fix: renomeia env.template.js para src/assets/env.js e ajusta build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,5 @@
-declare global {
-  interface Window {
-    __env?: Record<string, any>;
-  }
-}
-
-const runtime: Record<string, any> =
-  typeof window !== 'undefined' && window.__env ? window.__env : {};
-
 export const environment = {
   production: false,
-  apiUrl: (runtime['NG_APP_API_BASE_URL'] as string),
+  apiUrl: (window as any).__env?.NG_APP_API_BASE_URL,
   // apiUrl: 'https://drainage-joke-territories-incorporate.trycloudflare.com',
 };
